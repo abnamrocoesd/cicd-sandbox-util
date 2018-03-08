0b7d64374d3f4926e8f7834e5ec3fe457ccc2197
Joost van der Griendt <j.vandergriendt@flusso.nl>
2018-03-08 20:41:12 +0100

    Make sure we also do the fmt stuff for the other packages

--- go-fmt-cleanup.sh
+++ go-fmt-cleanup.sh
@@ -2,3 +2,7 @@
 gofmt -s -w -l main.go
 gofmt -s -w -l model/*.*
 gofmt -s -w -l webserver/*.*
+gofmt -s -w -l jenkins/*.*
+gofmt -s -w -l dockerprobe/*.*
+gofmt -s -w -l sonarqube/*.*
+gofmt -s -w -l util/*.*
