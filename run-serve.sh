0af64449943b2e7ade30c2e29602f5108d6cac3c
Joost van der Griendt <j.vandergriendt@flusso.nl>
2018-03-11 15:49:54 +0100

    Give the container a name

--- run-serve.sh
+++ run-serve.sh
@@ -1,5 +1,6 @@
 docker run --rm -p 7777:7777 \
     -v /var/run/docker.sock:/var/run/docker.sock\
+    --name cicd-sandbox-util-serve\
     abnamrocoesd/cicd-sandbox-util:latest cicd-util\
     -action serve\
     -labelPrefix "com.github.joostvdg."\
